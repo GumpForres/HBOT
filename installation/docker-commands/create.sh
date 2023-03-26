c5c907035008db5a461163d9341f3166b4f4f3a1
GumpForres <89737286+GumpForres@users.noreply.github.com>
2023-03-26 21:13:38 +0800

    docker-commands

--- installation/docker-commands/create.sh
+++ installation/docker-commands/create.sh
@@ -3,31 +3,31 @@
 
 echo
 echo
-echo "===============  CREATE A NEW HUMMINGBOT INSTANCE ==============="
+echo "===============  CREATE A NEW HBOT INSTANCE ==============="
 echo
 echo
 echo "ℹ️  Press [ENTER] for default values:"
 echo
 
 # Specify hummingbot version
-read -p "   Enter Hummingbot version you want to use [latest/development] (default = \"latest\") >>> " TAG
+read -p "   Enter Hbot version you want to use [latest/development] (default = \"latest\") >>> " TAG
 if [ "$TAG" == "" ]
 then
   TAG="latest"
 fi
 
 # Ask the user for the name of the new instance
-read -p "   Enter a name for your new Hummingbot instance (default = \"hummingbot\") >>> " INSTANCE_NAME
+read -p "   Enter a name for your new Hbot instance (default = \"hbot\") >>> " INSTANCE_NAME
 if [ "$INSTANCE_NAME" == "" ]
 then
-  INSTANCE_NAME="hummingbot"
-  DEFAULT_FOLDER="hummingbot_files"
+  INSTANCE_NAME="hbot"
+  DEFAULT_FOLDER="hbot_files"
 else
   DEFAULT_FOLDER="${INSTANCE_NAME}_files"
 fi
 
 # Ask the user for the folder location to save files
-read -p "   Enter a folder name where your Hummingbot files will be saved (default = \"$DEFAULT_FOLDER\") >>> " FOLDER
+read -p "   Enter a folder name where your Hbot files will be saved (default = \"$DEFAULT_FOLDER\") >>> " FOLDER
 if [ "$FOLDER" == "" ]
 then
   FOLDER=$PWD/$DEFAULT_FOLDER
@@ -45,7 +45,7 @@ echo
 echo "ℹ️  Confirm below if the instance and its folders are correct:"
 echo
 printf "%30s %5s\n" "Instance name:" "$INSTANCE_NAME"
-printf "%30s %5s\n" "Version:" "hummingbot/hummingbot:$TAG"
+printf "%30s %5s\n" "Version:" "hbot:$TAG"
 echo
 printf "%30s %5s\n" "Main folder path:" "$FOLDER"
 printf "%30s %5s\n" "Config files:" "├── $CONF_FOLDER"
@@ -67,11 +67,11 @@ prompt_proceed () {
 # Execute docker commands
 create_instance () {
  echo
- echo "Creating Hummingbot instance ... Admin password may be required to set the required permissions ..."
+ echo "Creating Hbot instance ... Admin password may be required to set the required permissions ..."
  echo
  # 1) Create main folder for your new instance
  mkdir $FOLDER
- # 2) Create subfolders for hummingbot files
+ # 2) Create subfolders for hbot files
  mkdir $CONF_FOLDER
  mkdir $CONF_FOLDER/connectors
  mkdir $CONF_FOLDER/strategies
@@ -80,9 +80,9 @@ create_instance () {
  mkdir $PMM_SCRIPTS_FOLDER
  mkdir $CERTS_FOLDER
  mkdir $SCRIPTS_FOLDER
- # 3) Set required permissions to save hummingbot password the first time
+ # 3) Set required permissions to save hbot password the first time
  sudo chmod a+rw $CONF_FOLDER $CERTS_FOLDER
- # 4) Launch a new instance of hummingbot
+ # 4) Launch a new instance of hbot
  docker run -it --log-opt max-size=10m --log-opt max-file=5 \
  --name $INSTANCE_NAME \
  --network host \
@@ -92,7 +92,7 @@ create_instance () {
  -v $PMM_SCRIPTS_FOLDER:/pmm_scripts \
  -v $SCRIPTS_FOLDER:/scripts \
  -v $CERTS_FOLDER:/certs \
- hummingbot/hummingbot:$TAG
+ hbot:$TAG
 }
 
 prompt_proceed
